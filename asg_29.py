b61ff9683e4bcaa8299c1d4a462f791b0953e450
Nirajan Dhakal <nirajan.data@gmail.com>
2021-02-15 14:15:41 +0545

    dict

--- asg_29.py
+++ asg_29.py
@@ -1,23 +1,6 @@
-from turtle import Turtle, Screen
-
-def draw_petal(turtle, radius):
-    heading = turtle.heading()
-    turtle.speed(30)
-    turtle.color("Red")
-    turtle.circle(radius, 60)
-    turtle.left(120)
-    turtle.circle(radius, 60)
-    turtle.setheading(heading)
-
-my_radius = int(input("What is the radius of the flower? "))
-my_petals = int(input("How many petals do you want? "))
-
-bob = Turtle()
-
-for _ in range(my_petals):
-    draw_petal(bob, my_radius)
-    bob.left(360 / my_petals)
-
-bob.hideturtle()
-
-screen = Screen()
+d1={"a":"nir","b":"aja"}
+d2={"v":"jan"}
+d3={"k":"dhakal"}
+d4={}
+for a in (d1,d2,d3):d4.update(a)
+print(d4)
