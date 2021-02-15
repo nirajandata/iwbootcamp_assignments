15057dba8699e9941ab1534717ff76fd019a7d5a
Nirajan Dhakal <nirajan.data@gmail.com>
2021-02-15 14:45:12 +0545

    d....

--- asg_3.py
+++ asg_3.py
@@ -1,10 +1,10 @@
-def change_char(str1):
+def firstval(str1):
   char = str1[0]
   str1 = str1.replace(char, '$')
   print(str1)
-  
+
   str1 = char + str1[1:]
    
   return str1
 
-print(change_char('restart'))
+print(firstval('restart'))
---
+++ asg_30.py
@@ -0,0 +1,8 @@
+d={1:20,2:40,3:60}
+def ch(x):
+    if x in d:
+        print("key is there")
+    else:
+        print("not key")
+
+ch(4)
